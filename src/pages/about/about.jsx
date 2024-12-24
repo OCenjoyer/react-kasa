998b5ac820e48e7aee8c7f730b6a966cbabb04b7
OCenjoyer <dupouy.kenzi@gmail.com>
2024-12-24 12:32:56 +0100

    Responsive du footer

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import AboutBanner from "../../components/abt_banner/Abt_Banner";
 import Collapse from "../../components/collapse/Collapse";
 import deroulant from "../../logements/deroulant.json"; 
+import './about.scss';
 
 export default function About() {
-	return (
-		<div>
-			<AboutBanner />
-			{deroulant.map((rule, index) => (
-				<Collapse key={index} aboutTitle={rule.aboutTitle} aboutText={rule.aboutText} />
-			))}
-		</div>
-	);
+    return (
+        <div className="wrapper">
+            <AboutBanner />
+            <div className="content">
+                {deroulant.map((rule, index) => (
+                    <Collapse key={index} aboutTitle={rule.aboutTitle} aboutText={rule.aboutText} />
+                ))}
+            </div>
+        </div>
+    );
 }
