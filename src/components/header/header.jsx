b242d2e2794d1eae9e72d11c82ceba63170652aa
OCenjoyer <dupouy.kenzi@gmail.com>
2024-12-24 10:38:05 +0100

    Responsive du Nav

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from 'react';
 import logo from '../../assets/image/LOGO.svg';
-import Nav from '../nav/Nav'
+import Nav from '../nav/Nav';
 import './Header.scss';
 
-export default function header() {
+export default function Header() {
   return (
-		<header className="headerWrap">
-			<figure className='headerWrap__fig'>
-				<img className="logo" src={logo} alt="logo de l'agence kasa" />
-			</figure>
-			<Nav className="nav-header" />
-		</header>
-	)
+    <header className="headerWrap">
+      <figure className='headerWrap__fig'>
+        <img className="logo" src={logo} alt="logo de l'agence kasa" />
+      </figure>
+      <Nav className="nav-header" />
+    </header>
+  );
 }
