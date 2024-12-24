bb8db266bd1a96fea7d8306341b818124165cc10
OCenjoyer <dupouy.kenzi@gmail.com>
2024-12-24 09:50:24 +0100

    debut responsive

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -3,18 +3,19 @@ import Banner from "../../components/banner/banner";
 import Card from "../../components/card/card";
 import { Link } from "react-router-dom";
 import data from "../../logements/logements.json";
+import './home.scss';
 
 export default function Home() {
-	return (
-		<div>
-			<Banner />
-			<div className="cards-container">
-				{data.map(({ id, cover, title }) => (
-					<Link className="link_card_logement" to={`/logement/${id}`} key={id}>
-						<Card cover={cover} title={title} />
-					</Link>
-				))}
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <Banner />
+      <div className="cards-container">
+        {data.map(({ id, cover, title }) => (
+          <Link className="link_card_logement" to={`/logement/${id}`} key={id}>
+            <Card cover={cover} title={title} />
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 }
